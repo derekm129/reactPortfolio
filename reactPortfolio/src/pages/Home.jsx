01af6cef9317a552439e5ee2aa2d72dae451ee26
dmod129 <derek.modugno@gmail.com>
2024-01-07 15:34:02 -0600

    added images and styling

--- reactPortfolio/src/pages/Home.jsx
+++ reactPortfolio/src/pages/Home.jsx
@@ -1,26 +1,31 @@
+import horisean from '../assets/images/horisean.png';
+import plantPic from '../assets/images/plantScreenshot.png';
+import project1 from '../assets/images/project1Page.png';
+import '../App.css';
+
 export default function Home() {
     return (
       <div>
         <h1>My Portfolio</h1>
-          <section class="project"> 
+          <section className="project"> 
             <a href="https://derekm129.github.io/horiseon-refactor/#social-media-marketing">
-                <img src="./assets/images/screenshot1.png" id="horisean" className="float-left" />
+                <img src={horisean} id="horisean" className="float-left" />
             </a>
             <h3>Horisean Refactor</h3>
               <p>For this project I refactored code for a webpage in order to make it more accessible. I renamed and combined HTML and CSS elements in order to make the code more efficient.</p>
           </section>
 
-          <section class="project">
+          <section className="project">
             <a href="https://nameless-wave-14504-8d3cae4aaa8e.herokuapp.com/">
-              <img src="./assets/images/plantScreenshot.png" id="project-pictures" className="float-right"/>
+              <img src={plantPic} id="project-pictures" className="float-right"/>
             </a>
             <h3>Plant Parenthood</h3>
               <p>For this group project I worked mostly on the backend for a website that lets users search for plant information by entering the name of a plant in the search bar.</p>
             </section>
 
-            <section class="project">
+            <section className="project">
                 <a href="https://lildano50.github.io/group-project-1/"> 
-                  <img src="./assets/images/project1Page.png" id="project-pictures" className="float-left"/>
+                  <img src={project1} id="project-pictures" className="float-left"/>
                 </a>
                 <h3>What to Do</h3>
                   <p>This was my first group project I worked on at the UT Austin Coding Bootcamp. This website allows a user to enter a zip code in order to find random activities to do in their area. I helped make this app along with Daniel Park, Phuc Dinh, and Dasarathan T.</p>
