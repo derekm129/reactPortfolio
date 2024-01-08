04966a68fbb19ae468a5f8be6e0113e0d977fad5
dmod129 <derek.modugno@gmail.com>
2024-01-08 15:22:03 -0600

    got contact page to work

--- reactPortfolio/src/pages/Contact.jsx
+++ reactPortfolio/src/pages/Contact.jsx
@@ -1,15 +1,32 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_g1hqmlj', 'template_qexml4u', form.current, 'sK88DBx3PdafeLl3C')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
     return (
       <div>
         <h1>Contact</h1>
        <section>
           <div className="container mb-3">
-            <form className="form-control card flex-center dir-column p-1">
+            <form ref={form} onSubmit={sendEmail} className="form-control card flex-center dir-column p-1">
               <input type="text" placeholder="Full Name" name="user_name" className="m-1" required />
               <input type="text" placeholder="Email" name="user_email" className="m-1" required />
               <input type="text" placeholder="Subject" name="user_subject" className="m-1" required />
               <textarea name="message" cols="30" rows="10" className="m-1"></textarea>
-              <button type="submit" className="--btn--btn-primary m-1">Send Message</button>
+              <button type="submit" value="Send" className="--btn--btn-primary m-1">Send Message</button>
             </form>
           </div>
    
