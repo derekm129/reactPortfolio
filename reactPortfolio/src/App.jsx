e02646766601ce0fe99d2cb3041ca709c0302fd8
dmod129 <derek.modugno@gmail.com>
2024-01-09 15:47:25 -0600

    created header

--- reactPortfolio/src/App.jsx
+++ reactPortfolio/src/App.jsx
@@ -1,16 +1,16 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
+import Header from './components/Header';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
-    <>
-      <Nav />
+    <div className="container flex-column justify-flex-start min-100-vh">
+      <Header />
       <main className="mx-3">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
