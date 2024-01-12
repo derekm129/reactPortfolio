5e35a5401758907cb8c8e3e08668053ad9d8edcc
dmod129 <derek.modugno@gmail.com>
2024-01-12 17:26:57 -0600

    made resume pdf downloadable

--- reactPortfolio/src/pages/Resume.jsx
+++ reactPortfolio/src/pages/Resume.jsx
@@ -1,45 +1,20 @@
-export default function Resume() {
-    return (
-      <div>
-        <h1>Resume</h1>
-        <h3>Skills</h3>
-          <ul>
-            <li>Advanced Computer Knowledge and Troubleshooting</li>
-            <li>Operations and Management Experience</li>
-            <li>Proficient Training and Delegation Skills</li>
-            <li>Excellent Communication Skills</li>
-            <li>Proven Team Player</li>
-            <li>Excellent Customer Service</li>
-          </ul>
-        <h3>Technology Summary</h3>
-          <h4>Computer Systems</h4>
-            <ul>
-              <li>Windows</li>
-              <li>DOS</li>
-            </ul>
-          <h4>Programming Languages</h4>
-            <ul>
-              <li>Javascript</li>
-              <li>React JS</li>
-              <li>HTML</li>
-              <li>CSS</li>
-            </ul>
-          <h4>Databases</h4>
-            <ul>
-              <li>MySQL</li>
-              <li>MongoDB</li>
-            </ul>
-        <h3>Education</h3>
-          <ul>
-            <li>Bachelor's of Arts, Indiana University, 2014</li>
-          </ul>
-        <h3>Certifications</h3>
-          <ul>
-            <li>UTA Coding Bootcamp Certification, 2024</li>
-          </ul>
-        <p>
-        </p>
-      </div>
-    );
-  }
-  
+import ResumePDF from '../utils/DerekModugno_Resume2024.pdf';
+
+function App () {
+  return (
+    <div>
+      <h2>Resume</h2>
+
+      <a
+        href={ResumePDF}
+        download="DerekModugno_Resume2024"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button>Download .pdf file</button>
+      </a>
+    </div>
+  )
+}
+
+export default App;
