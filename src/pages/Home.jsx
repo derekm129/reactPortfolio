c7cbaf31b8ee208f9a228e98c07c4baf3881b56d
dmod129 <derek.modugno@gmail.com>
2024-01-30 14:25:21 -0600

    added gitHub links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Project from '../components/Project';
 
 // Project Images
 import horisean from '../assets/images/horisean.png';
-import plantPic from '../assets/images/plantScreenshot.png';
+import syntax from '../assets/images/syntaxScreenshot.png';
 import project1 from '../assets/images/project1Page.png';
 import ecommerce from '../assets/images/e-commerce.png';
 import employeeTracker from '../assets/images/employeeTracker.png';
@@ -20,27 +20,32 @@ export default function Home() {
         title={"Horisean Refactor"}
       />
       <Project
-        url={"https://nameless-wave-14504-8d3cae4aaa8e.herokuapp.com/"}
-        body={plantPic}
+        url={"https://syntax-8rz6.onrender.com/"}
+        deployment={"https://github.com/grayd500/Syntax"}
+        body={syntax}
         title={"Plant Parenthood"}
       />
       <Project
         url={"https://lildano50.github.io/group-project-1/"}
+        deployment={"https://github.com/lildano50/group-project-1"}
         body={project1}
         title={"What to Do"}
       />
       <Project
         url={"https://github.com/derekm129/E-commerce-Back-End"}
+        deployment={"https://github.com/derekm129/E-commerce-Back-End"}
         body={ecommerce}
         title={"E-Commerce Backend"}
       />
       <Project
         url={"https://github.com/derekm129/Employee-Tracker"}
+        deployment={"https://github.com/derekm129/Employee-Tracker"}
         body={employeeTracker}
         title={"Employee Tracker"}
       />
       <Project
         url={"https://derekm129.github.io/calendarApp/"}
+        deployment={"https://github.com/derekm129/calendarApp"}
         body={calendar}
         title={"Calendar App"}
       />
