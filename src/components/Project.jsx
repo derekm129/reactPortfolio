7091278c9b5d4ef7bde8d838612a62da8b3b5db4
dmod129 <derek.modugno@gmail.com>
2024-02-23 12:41:45 -0600

    changed color and style

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,10 +7,10 @@ function Project(props) {
       <div className="project row justify-content-center align-items-center">
         {/* Title */}
         <a href={props.deployment}>
-          <h3 className='project-title text-center'>{props.title}</h3>
+          <h3 className='project-title text-center' style={{ color: 'white' }}>{props.title}</h3>
         </a>
         {/* Body */}
-        <div className='anotherContainer'>
+        <div className='anotherContainer' style={{ boxShadow: '0px 0px 5px 5px limegreen' }}>
           <a href={props.url}>
             <img src={props.body} id="project-pictures" className="project-pictures img-fluid" />
           </a>
