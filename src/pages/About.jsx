a9fa47d70c6a89ee2810e2f3ffd9c458c1b8a9b8
dmod129 <derek.modugno@gmail.com>
2024-07-24 17:02:15 -0500

    added button effects

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,27 +1,29 @@
-import profilePic from '../assets/images/DerekLinkdin_Final.png'
+import profilePic from '../assets/images/DerekLinkdin_Final.png';
+import '../App.css';
+
 
 export default function About() {
   return (
     <div className='container text-center'>
       <div className='row justify-content-center align-items-center'>
         {/* Bio */}
-        <div className='col-md-6'>
-          <p className='m-3' style={{ color: 'white', boxShadow: '0px 0px 3px 3px limegreen' }}>
+        <div className='col-md-6 mt-5'>
+          <p className='m-3' style={{ color: 'white', boxShadow: '0px 0px 3px 3px limegreen', fontSize: '1.7em' }}>
             Hello, I'm Derek Modugno and I'm a fullstack web developer skilled with several technologies including JavaScript, CSS, and HTML. I have a Web Development Bootcamp Certificate from the
             University of Texas, Austin, and a Bachelor's Degree from Indiana University, Bloomington. I am a creative problem-solver and team player with a passion for developing apps and learning new technologies.
           </p>
           {/* Buttons */}
           <div className="buttons mt-3">
-            <a href="https://www.linkedin.com/in/derek-modugno-9927b72b1/" className='mb-2'>
-              <button>View Linkedin</button>
+            <a href="https://www.linkedin.com/in/derek-modugno-9927b72b1/" className='mb-2 m-2'>
+              <button className='btn btn-lg bg-primary'>View Linkedin</button>
             </a>
-            <a href="https://github.com/derekm129" className='mb-2'>
-              <button>View GitHub</button>
+            <a href="https://github.com/derekm129" className='mb-2 m-2'>
+              <button className='btn btn-lg bg-secondary'>View GitHub</button>
             </a>
           </div>
         </div>
         {/* Profile Picture */}
-        <div className='col-md-4'>
+        <div className='col-md-4 mt-4'>
           <img src={profilePic} id="profilePic" style={{ boxShadow: '0px 0px 3px 3px limegreen' }} />
         </div>
       </div>
