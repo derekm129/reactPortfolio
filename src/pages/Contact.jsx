3301ce9e2dcb082b59d40aab6dec11ddabcad99f
dmod129 <derek.modugno@gmail.com>
2024-07-24 21:25:06 -0500

    changed contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import '../App.css';
+
 
 
 export default function Contact() {
@@ -21,25 +21,27 @@ export default function Contact() {
   };
   // Contact Page
   return (
-    <div>
-      <h1>Contact</h1>
-      <section>
-        <div className="container mb-3">
-          <form ref={form} onSubmit={sendEmail} className="form-control card flex-center dir-column p-1">
-            {/* Name */}
-            <input type="text" placeholder="Full Name" name="user_name" className="m-1" required />
-            <input type="text" placeholder="Email" name="user_email" className="m-1" required />
-            {/* Subject */}
-            <input type="text" placeholder="Subject" name="user_subject" className="m-1" required />
-            {/* Text Area */}
-            <textarea name="message" cols="30" rows="10" className="m-1" ></textarea>
-            {/* Submit Button */}
-            <button type="submit" value="Send" className="--btn--btn-primary m-1">Send Message</button>
-          </form>
-        </div>
-
-      </section>
+    <div className="d-flex justify-content-center align-items-center">
+      <div className="card p-1 mt-5" style={{ width: '100%', maxWidth: '500px', height: 'auto', minHeight: '400px' }}>
+        <h1 className="mb-2 text-dark text-center">Contact Me</h1>
+        <section>
+          <div>
+            <form ref={form} onSubmit={sendEmail} className="form-control card flex-center dir-column p-1">
+              {/* Name */}
+              <input type="text" placeholder="Full Name" name="user_name" className="m-1" required />
+              <input type="text" placeholder="Email" name="user_email" className="m-1" required />
+              {/* Subject */}
+              <input type="text" placeholder="Subject" name="user_subject" className="m-1" required />
+              {/* Text Area */}
+              <textarea name="message" cols="30" rows="10" className="m-1" ></textarea>
+              {/* Submit Button */}
+              <button type="submit" value="Send" className="--btn--btn-primary m-1">Send Message</button>
+            </form>
+          </div>
+        </section>
+      </div>
     </div>
+
   );
 }
 
